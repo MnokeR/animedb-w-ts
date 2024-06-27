4237ff77afa644b2faa0241baa6de80fe9e55aab
MnokeR <mnoker@gmail.com>
2024-06-27 16:00:58 -0700

    installed and setup @tanstack query and react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+const router = createBrowserRouter([]);
+
 function App() {
-  return "";
+  <QueryClientProvider client={queryClient}>
+    <RouterProvider router={router} />;
+  </QueryClientProvider>;
 }
 
 export default App;
